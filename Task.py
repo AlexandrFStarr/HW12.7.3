d0b473df3fd9618f3623e2c03b1a936842368637
unknown <shekhovtsov@mail2000.ru>
2023-04-27 00:51:59 +0700

    Initial commit

---
+++ Task.py
@@ -0,0 +1,27 @@
+# num = int(input())
+# data = []
+# while num != 0:
+#     data.append(num)
+#     num = int(input())
+# data.sort()
+# print(data)
+
+# words = input()
+# data = []
+# while words != '':
+#     if words not in data:
+#         data.append(words)
+#     words = input()
+#
+# for item in data:
+#     print(item)
+#
+
+data = [1, 2, 3, 4, 5, 6, 7, 8]
+for item in data:
+     if item % 2 == 0:
+         print(item)
+
+
+
+
